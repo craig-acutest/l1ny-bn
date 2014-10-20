b1d03d8fc0273ea1c0339d36d63d6b5d5cf7ac8d
craig-acutest <craig.williams@acutesales.co.uk>
2014-10-20 14:22:11 +0100

    init commit

---
+++ l1ny/l1ny/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="l1ny.MvcApplication" Language="C#" %>
---
+++ l1ny/l1ny/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(l1ny.Startup))]
+namespace l1ny
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
